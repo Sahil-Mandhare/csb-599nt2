634c352fb537c3dd2a76576c719fc2f1419cff3e
Code Sandbox <hello@codesandbox.io>
2023-09-25 17:56:19 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.0a08db8050af4c7572d765777611bbe3.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96366caeb96469e414959faba03b82a",
+    "url": "https://sahil-mandhare.github.io/csb-599nt2/index.html"
+  },
+  {
+    "revision": "333ae1545be604ef8ace",
+    "url": "https://sahil-mandhare.github.io/csb-599nt2/static/js/main.914fe783.chunk.js"
+  },
+  {
+    "revision": "347178bdcc4479aec8a6",
+    "url": "https://sahil-mandhare.github.io/csb-599nt2/static/js/runtime-main.b0cfbc66.js"
+  }
+]);
